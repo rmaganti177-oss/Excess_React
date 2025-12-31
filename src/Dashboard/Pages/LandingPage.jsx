5e1c00086767924cfae73100488af58d6b6b44d3
rmaganti177-oss <your-github-email@example.com>
2025-12-31 07:22:28 +0530

    Initial code

---
+++ src/Dashboard/Pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../componets/Navbar'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ src/Dashboard/componets/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='Mark'>
+        <h2>Navbar</h2>
+        <p>Login</p>
+    </div>
+  )
+}
+
+export default Navbar
